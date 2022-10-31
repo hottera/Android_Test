62e518b5f78826040dc3426df90f4ce7de3edd30
Hotter Anton <Fenstersteuerungber>
2022-10-31 21:41:14 +0100

    second push

---
+++ app/src/main/java/com/example/helloworldtest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.helloworldtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//basic java code for main activity
+
+public class MainActivity extends AppCompatActivity {
+
+    //defines what happen when the activity is started
+    //initializes a default view from activity main
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void foo(){
+        System.out.println("just for pushing purpose");
+    }
+}
